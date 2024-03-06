ca1374f49ee3706c8166be41fdb7a2d73d449e73
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-03-06 16:54:45 +0500

    create initial pages, add icons

--- Inflow.Mobile/Inflow.Mobile/Inflow.Mobile.Android/MainActivity.cs
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using FFImageLoading.Forms.Platform;
 
 namespace Inflow.Mobile.Droid
 {
@@ -16,6 +17,10 @@ namespace Inflow.Mobile.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
+
+            CachedImageRenderer.Init(true);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
--- Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/ViewModels/AboutViewModel.cs
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/ViewModels/AboutViewModel.cs
@@ -7,10 +7,25 @@ namespace Inflow.Mobile.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        private string _counter;
+        public string Counter
+        {
+            get => _counter;
+            set => SetProperty(ref _counter, value);
+        }
+
+        public ICommand CounterUpdateCommand { get; }
+
         public AboutViewModel()
         {
             Title = "About";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            CounterUpdateCommand = new Command(OnUpdateCounter);
+        }
+
+        private void OnUpdateCounter()
+        {
+            Counter += 1;
         }
 
         public ICommand OpenWebCommand { get; }
