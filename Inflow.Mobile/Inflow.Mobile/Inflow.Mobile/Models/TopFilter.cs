14b5d46c80557d752f3ff681d353007184f6c75b
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-03-12 15:17:14 +0500

    Updated Home Page

---
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/Models/TopFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inflow.Mobile.Models
+{
+    internal class TopFilter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public TopFilter(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
--- Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/ViewModels/HomeViewModel.cs
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/ViewModels/HomeViewModel.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using Inflow.Mobile.Models;
+using Inflow.Mobile.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Inflow.Mobile.ViewModels
 {
     internal class HomeViewModel : BaseViewModel
     {
+        private IDataStore<Product> _dataStore;
+        public ObservableCollection<TopFilter> TopFilters { get; set; }
+
+        public ObservableCollection<Product> Products { get; set; }
+
+        public HomeViewModel()
+        {
+            Title = "Home";
+            TopFilters = new ObservableCollection<TopFilter>()
+            {
+                new TopFilter(1, "All"),
+                new TopFilter(2, "Sales"),
+                new TopFilter(3, "Popular"),
+                new TopFilter(4, "Recommended"),
+            };
+
+            Products = new ObservableCollection<Product>();
+
+            _dataStore = new ProductDataStore();
+
+            LoadData();
+        }
+
+        public async Task LoadData()
+        {
+            var products = await _dataStore.GetItemsAsync();
+
+            foreach (var product in products)
+            {
+                Products.Add(product);
+            }
+        }
     }
 }
