126cfdad1569da1f2677f35e82b9020d42b2c60c
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-03-16 11:16:57 +0500

    Create Resopneses and DataStores

---
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/DataStores/Products/IProductDataStore.cs
@@ -0,0 +1,13 @@
+﻿using Inflow.Mobile.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inflow.Mobile.DataStores.Products
+{
+    public interface IProductDataStore
+    {
+        Task<GetProductsResponse> ProductsAsync();
+    }
+}
---
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/Services/ApiClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inflow.Mobile.Services
+{
+    public class ApiClient
+    {
+        private const string baseUrl = "https://localhost:7258/api";
+        private readonly HttpClient _client = new HttpClient();
+
+        public ApiClient()
+        {
+            _client.BaseAddress = new Uri(baseUrl);
+            _client.DefaultRequestHeaders.Add("Accept", "application/json");
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, _client.BaseAddress?.AbsolutePath + "/" + url);
+
+            var response = await _client.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error fetching url: {url}");
+            }
+
+            return response;
+        }
+    }
+}
