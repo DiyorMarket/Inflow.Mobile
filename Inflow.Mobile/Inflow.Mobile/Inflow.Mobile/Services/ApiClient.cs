42097b1037d5d6043bb3eb084961773e9faac2e0
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-03-22 12:11:25 +0500

    Upgrade Home Page
    1. Change colors.
    2. Improve Cards,
    3. Add Icons.
    4. Upgrade Menu TabBar

--- Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/Services/ApiClient.cs
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/Services/ApiClient.cs
@@ -8,7 +8,7 @@ namespace Inflow.Mobile.Services
 {
     public class ApiClient
     {
-        private const string BaseUrl = "https://vrq2qnlr-7258.asse.devtunnels.ms/api";
+        private const string BaseUrl = "https://vw2plztl-7258.asse.devtunnels.ms/api";
         private readonly HttpClient _client;
 
         public ApiClient()
