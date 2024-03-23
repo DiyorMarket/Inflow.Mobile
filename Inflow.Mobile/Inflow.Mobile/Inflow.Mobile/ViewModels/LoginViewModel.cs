ef15cea986c9b918bbe674660a0955f607ea008c
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-03-23 17:39:37 +0500

    Create Login, Register pages

--- Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/ViewModels/LoginViewModel.cs
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/ViewModels/LoginViewModel.cs
@@ -1,24 +1,88 @@
-﻿using Inflow.Mobile.Views;
+﻿using Inflow.Mobile.Services;
+using Inflow.Mobile.Views;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Inflow.Mobile.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
-        public Command LoginCommand { get; }
+        private readonly LoginService _loginService;
 
-        public LoginViewModel()
+        private string _username;
+        public string Username
         {
-            LoginCommand = new Command(OnLoginClicked);
+            get => _username;
+            set => SetProperty(ref _username, value);
         }
 
-        private async void OnLoginClicked(object obj)
+        private string _phoneNumber;
+        public string PhoneNumber
         {
-            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            get { return _phoneNumber; }
+            set { SetProperty(ref _phoneNumber, value); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        private string _confirmPassword;
+        public string ConfirmPassword
+        {
+            get => _confirmPassword;
+            set => SetProperty(ref _confirmPassword, value);
+        }
+
+        private bool _showCancel;
+        public bool ShowCancel
+        {
+            get { return _showCancel; }
+            set { SetProperty(ref _showCancel, value); }
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand RegisterCommand { get; }
+        public ICommand ForgotPasswordCommand { get; }
+        public ICommand CancelLoginCommand { get; }
+
+        public LoginViewModel() 
+        {
+            LoginCommand = new Command(OnLogin);
+            RegisterCommand = new Command(OnRegister);
+
+            _loginService = new LoginService();
+        }
+
+        private async void OnLogin()
+        {
+            if (string.IsNullOrWhiteSpace(Username))
+            {
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(Password))
+            {
+                return;
+            }
+
+            var result = _loginService.LoginUser(Username, Password);
+
+            Application.Current.MainPage = new AppShell();
+        }
+
+        private async void OnRegister()
+        {
+
         }
     }
 }
--- Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/Views/LoginPage.xaml.cs
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/Views/LoginPage.xaml.cs
@@ -10,12 +10,22 @@ using Xamarin.Forms.Xaml;
 namespace Inflow.Mobile.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class LoginPage : ContentPage
+    public partial class LoginPage : CarouselPage
     {
         public LoginPage()
         {
             InitializeComponent();
             this.BindingContext = new LoginViewModel();
+        }
+
+        private void Register_Clicked(object sender, EventArgs e)
+        {
+            CurrentPage = Children[1];
+        }
+
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            CurrentPage = Children[0];
         }
     }
 }
