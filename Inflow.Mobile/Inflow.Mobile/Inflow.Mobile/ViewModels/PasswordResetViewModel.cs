f9854a2ed6451f2d1c76fb63f291f3d4d30193ff
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-04-02 17:22:02 +0500

    Create Reset PasswordViewModel

---
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/ViewModels/PasswordResetViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Inflow.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Inflow.Mobile.ViewModels
+{
+    public class PasswordResetViewModel : BaseViewModel
+    {
+        public ICommand LoginCommand { get; }
+
+        public PasswordResetViewModel() 
+        {
+            LoginCommand = new Command(OnLoginPage);
+        }
+
+        private void OnLoginPage(object obj)
+        {
+            var loginPage = new LoginPage();
+
+            Application.Current.MainPage = loginPage;
+        }
+    }
+}
