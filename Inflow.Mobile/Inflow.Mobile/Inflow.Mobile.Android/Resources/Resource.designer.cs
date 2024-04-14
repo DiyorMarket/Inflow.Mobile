f8c2e199f0d378c5956ed83de7764f9d01044c6e
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-04-14 16:13:41 +0500

    Updated_code

--- Inflow.Mobile/Inflow.Mobile/Inflow.Mobile.Android/Resources/Resource.designer.cs
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile.Android/Resources/Resource.designer.cs
@@ -23739,34 +23739,37 @@ namespace Inflow.Mobile.Droid
 			public const int preference_list_divider_material = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int red_heart = 2131165347;
+			public const int recycle_bin = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int register = 2131165348;
+			public const int red_heart = 2131165348;
 			
 			// aapt resource value: 0x7F0700A5
-			public const int shopping = 2131165349;
+			public const int register = 2131165349;
 			
 			// aapt resource value: 0x7F0700A6
-			public const int telephone = 2131165350;
+			public const int shopping = 2131165350;
 			
 			// aapt resource value: 0x7F0700A7
-			public const int test_custom_background = 2131165351;
+			public const int telephone = 2131165351;
 			
 			// aapt resource value: 0x7F0700A8
-			public const int tooltip_frame_dark = 2131165352;
+			public const int test_custom_background = 2131165352;
 			
 			// aapt resource value: 0x7F0700A9
-			public const int tooltip_frame_light = 2131165353;
+			public const int tooltip_frame_dark = 2131165353;
 			
 			// aapt resource value: 0x7F0700AA
-			public const int user = 2131165354;
+			public const int tooltip_frame_light = 2131165354;
 			
 			// aapt resource value: 0x7F0700AB
-			public const int xamarin_logo = 2131165355;
+			public const int user = 2131165355;
 			
 			// aapt resource value: 0x7F0700AC
-			public const int yellow_cart = 2131165356;
+			public const int xamarin_logo = 2131165356;
+			
+			// aapt resource value: 0x7F0700AD
+			public const int yellow_cart = 2131165357;
 			
 			static Drawable()
 			{
