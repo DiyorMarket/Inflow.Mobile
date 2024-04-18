50021ec44cd9e1e20106c9ba322200959a3cbeea
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-04-18 15:23:00 +0500

    Create AuthenticationResponse class

---
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/Models/AuthenticationResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inflow.Mobile.Models
+{
+    public class AuthenticationResponse
+    {
+        public string Token { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPhone { get; set; }
+    }
+}
