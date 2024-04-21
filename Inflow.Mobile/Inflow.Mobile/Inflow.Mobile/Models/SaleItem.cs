912ca736d83c88a1f20d2d088da652109bf1bb1d
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-04-21 11:43:00 +0500

    UpdateHistoryView

--- Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/Models/SaleItem.cs
+++ Inflow.Mobile/Inflow.Mobile/Inflow.Mobile/Models/SaleItem.cs
@@ -4,8 +4,7 @@
     {
         public string ProductName { get; set; }
         public int Quantity { get; set; }
-        public decimal TotalPrice { get; set; }
-        public int ProductId { get; set; }
+        public decimal TotalDue { get; set; }
         public Product Product { get; set; }
     }
 }
